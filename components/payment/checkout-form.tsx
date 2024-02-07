e5aed222db18c4a75c2e52ab8d28f02bca7f8fc7
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-02-07 19:12:56 +0000

    Refactor payment form to use Stripe hooks and handle form submission - payments work

--- components/payment/checkout-form.tsx
+++ components/payment/checkout-form.tsx
@@ -1,11 +1,37 @@
-import { PaymentElement } from '@stripe/react-stripe-js';
 import { Button } from '@/components/ui/button';
+import {
+  useStripe,
+  useElements,
+  PaymentElement
+} from '@stripe/react-stripe-js';
 
 export const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    // prevent the default form submission
+    event.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: window.location.href
+      }
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
-      <Button>Submit</Button>
+      <Button
+        type="submit"
+        disabled={!stripe}
+      >
+        Submit
+      </Button>
     </form>
   );
 };
