bd4af5058678a6bdfbe3634a43e03aea8ec89096
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-02-26 09:00:07 +0000

    adds more comments for the user auth middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -8,51 +8,55 @@ export async function updateSession(request: NextRequest) {
     }
   });
 
-  const supabase = createServerClient(
-    'https://bixpqirmnvxjgpfjhzrk.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpeHBxaXJtbnZ4amdwZmpoenJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NzU4ODgsImV4cCI6MjAyMjQ1MTg4OH0.OSavv0-02TFPk4pBZx9yK0AXtbQyuibkvmqaGYD-tSo',
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers
-            }
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers
-            }
-          });
-          response.cookies.set({
-            name,
-            value: '',
-            ...options
-          });
-        }
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+  // we need the env variables to be set
+  if (!supabaseUrl || !supabaseKey)
+    throw new Error('Missing env variables for Supabase');
+
+  // this is used in this file to access the user
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      get(name: string) {
+        return request.cookies.get(name)?.value;
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value,
+          ...options
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers
+          }
+        });
+        response.cookies.set({
+          name,
+          value,
+          ...options
+        });
+      },
+      remove(name: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value: '',
+          ...options
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers
+          }
+        });
+        response.cookies.set({
+          name,
+          value: '',
+          ...options
+        });
       }
     }
-  );
+  });
 
   await supabase.auth.getUser();
 
