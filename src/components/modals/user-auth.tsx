044f00ef76e11bbd4004b16ac16bc888278529fa
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-02 22:22:38 +0000

    feat: added prop to DialogContent to hide/show close button in dialog.tsx

--- src/components/modals/user-auth.tsx
+++ src/components/modals/user-auth.tsx
@@ -18,6 +18,12 @@ export default function UserAuthModal() {
   const pathname = usePathname();
   const router = useRouter();
 
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -26,34 +32,41 @@ export default function UserAuthModal() {
   }, [pathname]);
 
   if (pathname === '/sign-up') return '';
-  return (
-    <Dialog
-      modal={true}
-      defaultOpen={true}
-      open={true}
-    >
-      <DialogContent
-        hidden={false}
-        className="bg-black rounded-lg"
+  return isClient ? (
+    <>
+      <Dialog
+        modal={true}
+        defaultOpen={true}
+        open={true}
       >
-        <div className="flex flex-col gap-y-3 items-center">
-          <h2 className="text-3xl font-bold text-center">You need to log in</h2>
-          <p className="text-center">Click the button below to log in.</p>
-          <Button
-            className="border border-white w-fit px-6"
-            onClick={() => {
-              setLoading(true);
-              router.push('/sign-up');
-            }}
-          >
-            {loading ? (
-              <ReloadIcon className="w-3 h-3 animate-spin" />
-            ) : (
-              'Log in'
-            )}
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+        <DialogContent
+          hidden={false}
+          className="bg-black rounded-lg"
+          showCloseButton={false}
+        >
+          <div className="flex flex-col gap-y-3 items-center">
+            <h2 className="text-3xl font-bold text-center">
+              You need to log in
+            </h2>
+            <p className="text-center">Click the button below to log in.</p>
+            <Button
+              className="border border-white w-fit px-6"
+              onClick={() => {
+                setLoading(true);
+                router.push('/sign-up');
+              }}
+            >
+              {loading ? (
+                <ReloadIcon className="w-3 h-3 animate-spin" />
+              ) : (
+                'Log in'
+              )}
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  ) : (
+    ''
   );
 }
