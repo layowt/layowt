66cee4a0a2e08cc03284e10d5706e1bf636cafe9
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-03 17:35:30 +0000

    feat: 2FA modal created, start of listen to auth change events

--- src/components/modals/user-auth.tsx
+++ src/components/modals/user-auth.tsx
@@ -11,27 +11,52 @@ import { Button } from '@/components/ui/button';
 import { ReloadIcon } from '@radix-ui/react-icons';
 
 // use router as we are in a client component
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+
+// react imports
 import { useEffect, useState } from 'react';
 
+import { createClient } from '@/utils/supabase/client';
+import { CloudHail } from 'lucide-react';
+
 export default function UserAuthModal() {
+  const supabase = createClient();
+
   const pathname = usePathname();
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const [isClient, setIsClient] = useState(false);
-
   useEffect(() => {
     setIsClient(true);
+
+    // check if the user is logged in
+    const checkUser = async () => {
+      const { data: user } = await supabase.auth.getUser();
+
+      console.log(user.user);
+      // if the user is present, clear the URL of the waiting_for_auth query param
+      if (user.user) {
+        router.replace('waiting_for_auth', undefined);
+      }
+    };
+
+    checkUser();
   }, []);
 
   const [loading, setLoading] = useState(false);
+  const [newUser, setNewUser] = useState(false);
 
   useEffect(() => {
     // Reset loading state when pathname changes
     setLoading(false);
+    // if the user is waiting for MFA, show the 'waiting for MFA' modal
+    if (searchParams.get('waiting_for_auth')) setNewUser(true);
   }, [pathname]);
 
+  // do not display the modal if the user is on the sign-up page
   if (pathname === '/sign-up') return '';
+
   return isClient ? (
     <>
       <Dialog
@@ -41,28 +66,34 @@ export default function UserAuthModal() {
       >
         <DialogContent
           hidden={false}
-          className="bg-black rounded-lg"
+          className="bg-black rounded-lg py-10"
           showCloseButton={false}
         >
-          <div className="flex flex-col gap-y-3 items-center">
-            <h2 className="text-3xl font-bold text-center">
-              You need to log in
-            </h2>
-            <p className="text-center">Click the button below to log in.</p>
-            <Button
-              className="border border-white w-fit px-6"
-              onClick={() => {
-                setLoading(true);
-                router.push('/sign-up');
-              }}
-            >
-              {loading ? (
-                <ReloadIcon className="w-3 h-3 animate-spin" />
-              ) : (
-                'Log in'
-              )}
-            </Button>
-          </div>
+          {newUser ? (
+            <DialogTitle className="text-3xl font-bold text-center">
+              Waiting for MFA
+            </DialogTitle>
+          ) : (
+            <div className="flex flex-col gap-y-3 items-center">
+              <h2 className="text-3xl font-bold text-center">
+                You need to log in
+              </h2>
+              <p className="text-center">Click the button below to log in.</p>
+              <Button
+                className="border border-white w-fit px-6"
+                onClick={() => {
+                  setLoading(true);
+                  router.push('/sign-up');
+                }}
+              >
+                {loading ? (
+                  <ReloadIcon className="w-3 h-3 animate-spin" />
+                ) : (
+                  'Log in'
+                )}
+              </Button>
+            </div>
+          )}
         </DialogContent>
       </Dialog>
     </>
--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -6,8 +6,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '';
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
 }
