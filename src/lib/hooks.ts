1b0ed640a8de4bdb86107fbb924a9b6ab2e34274
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-08 18:23:43 +0000

    fixes build errors

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -5,4 +5,4 @@ import type { RootState, AppDispatch, AppStore } from '@/store/store';
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
+//export const useAppStore: () => AppStore = useStore;
