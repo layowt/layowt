81d96b019f42bac1da6b119fc23308626d60276e
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-09 21:34:39 +0000

    adds loading spinner/billing period to pricing cards

--- src/components/payment/payment-card.tsx
+++ src/components/payment/payment-card.tsx
@@ -1,5 +1,7 @@
+import Stripe from 'stripe';
 // component imports
 import { PaymentButton } from '@/components/payment/payment-card-button';
+import { ReloadIcon } from '@radix-ui/react-icons';
 
 //type imports
 import type { StripeProduct } from '@/types/StripeProduct';
@@ -8,7 +10,15 @@ import type { StripeProduct } from '@/types/StripeProduct';
 import { Cairo } from 'next/font/google';
 const cairo = Cairo({ subsets: ['latin'] });
 
-export function PricingCard({ product }: { product: StripeProduct }) {
+export function PricingCard({
+  product,
+  isLoading,
+  billingPeriod
+}: {
+  product: StripeProduct;
+  isLoading: boolean;
+  billingPeriod: Stripe.PriceListParams.Recurring.Interval;
+}) {
   return (
     <>
       <div
@@ -41,15 +51,18 @@ export function PricingCard({ product }: { product: StripeProduct }) {
             <div className="flex gap-x-1 items-center">
               <div className="flex gap-x-1 items-center">
                 £
-                <span className={`text-3xl`}>
-                  {product.default_price?.unit_amount
-                    ? product.default_price?.unit_amount / 100
-                    : 'Free'}
-                </span>
+                {isLoading ? (
+                  <div className="">
+                    <ReloadIcon className="w-3 h-3 animate-spin" />
+                  </div>
+                ) : (
+                  <span className={`text-3xl`}>
+                    {product.default_price?.unit_amount / 100}
+                  </span>
+                )}
               </div>
-              <span className="text-xs mt-2">/ month</span>
+              <span className="text-xs mt-2">/ {billingPeriod}</span>
             </div>
-
             {/** Feature list */}
             <div className="flex flex-col gap-y-2">
               {product.features.map((feature) => (
