a7e45601ab19793ec0baa60ea5075e204819ef39
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-19 18:29:17 +0000

    fix: refactoring of modal code

--- src/components/user-auth.tsx
+++ src/components/user-auth.tsx
@@ -11,6 +11,8 @@ export default async function UserAuthentication({
 
   //console.log(user.user);
 
+  console.log(await supabase.auth.getSession());
+
   // if the user is not logged in, show a modal to allow the user to log in
   return (
     <>
