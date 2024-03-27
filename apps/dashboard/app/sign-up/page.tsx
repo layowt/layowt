82288d135f30b7c48e1679bce71fd063483a6b0d
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-27 21:44:24 +0000

    move sign-up into (auth)

--- apps/dashboard/app/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpComponent from '../../components/sign-up/sign-up-wrapper';
-
-export default function Signup() {
-  return (
-    <div className="font-karla">
-      <SignUpComponent />
-    </div>
-  );
-}
