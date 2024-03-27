c83b4cf1bd0ca9af11d3332c89efd6ddd423715d
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-27 22:40:01 +0000

    fixes prisma with turbo

--- apps/dashboard/utils/prisma.ts
+++ apps/dashboard/utils/prisma.ts
@@ -1,13 +1,2 @@
 import { PrismaClient } from "@prisma/client";
-
-// This declares a global variable prisma of type PrismaClient or undefined. 
-// this allows using prisma globally throughout the application.
-
-declare global {
-  var prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalThis.prisma || new PrismaClient();
-
-// This ensures that in development environments, changes to the Prisma client instance are reflected globally 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma; 
+export const prisma = new PrismaClient();
