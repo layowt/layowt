e5435501a42ab811c3b20399169221c0efa707bf
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-30 19:54:17 +0000

    feat: dashboard layout added

---
+++ apps/dashboard/app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import DashboardNavBar from '@/components/layout/navigation-navbar';
+import DashboardSidebar from '@/components/layout/navigation-sidebar';
+
+export default function DashboardLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-screen">
+      <div className="flex text-white font-kanit">
+        <DashboardSidebar />
+        <DashboardNavBar />
+      </div>
+
+      {children}
+    </div>
+  );
+}
