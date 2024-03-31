c6da0945f674e4ea3c64eb81788846e365afcb69
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-03-31 20:45:00 +0100

    feat: adds motion to logo & user modal

--- apps/dashboard/components/layout/navigation-sidebar.tsx
+++ apps/dashboard/components/layout/navigation-sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import NavigationItems from '@/components/layout/navigation-items';
 import SiteLogo from '@/components/logo';
+import { motion } from 'framer-motion';
 
 export default function DashboardSidebar() {
   return (
@@ -9,7 +10,13 @@ export default function DashboardSidebar() {
 					min-w-40 min-h-screen px-2 flex flex-col gap-y-10 text-white font-poppins border-r border-black-50
 				"
     >
-      <SiteLogo />
+      <motion.div
+        initial={{ opacity: 0, x: 0 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.1 }}
+      >
+        <SiteLogo />
+      </motion.div>
       <NavigationItems />
     </section>
   );
