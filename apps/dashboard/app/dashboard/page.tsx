0929632fb87b01b20743c6595adb29c8842125fa
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-01 00:03:18 +0100

    feat: adds toggle to notification icon

--- apps/dashboard/app/dashboard/page.tsx
+++ apps/dashboard/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
 export default function Dashboard() {
-  return <>DASHBOARD PAGE</>;
+  return (
+    <>
+      {/** TODO: MAKE THIS NAME COME FROM DB!  */}
+      <div className="text-2xl font-semibold font-inter text-white">
+        Welcome back, Logan!
+      </div>
+    </>
+  );
 }
