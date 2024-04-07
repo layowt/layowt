54a3deb87cc8915fc5d45d790112c6e191748123
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-07 18:39:47 +0100

    feat: start of making 'holding' create site page

---
+++ apps/dashboard/app/create-new-site/page.tsx
@@ -0,0 +1,28 @@
+import getUserFromSession from '@/utils/user/getUserFromSession';
+import { prisma } from '@/utils/prisma';
+
+export default async function CreateNewSite() {
+  // 1. check if the user has reached their limit of sites
+  // 2. generate a new site id
+  // 3. redirect to the 'site-builder/[siteUid]' page
+
+  // get the userId from the current session
+  const userSession = await getUserFromSession();
+
+  if (!userSession || !userSession.data.user.id) {
+    return;
+  }
+  // get the user from the db
+  const user = await prisma.users.findFirst({
+    where: {
+      uid: userSession.data.user.id
+    }
+  });
+
+  return (
+    <div className="text-white">
+      <h1>{userSession.data.user.id}</h1>
+      <h3>{user.email}</h3>
+    </div>
+  );
+}
