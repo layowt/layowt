c3c844c63be70155cd4756d04c36507ad84bc415
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-12 20:45:35 +0100

    fix: new util to get user subscription

---
+++ apps/dashboard/utils/subscriptions/subscriptions.get.ts
@@ -0,0 +1,11 @@
+import { prisma } from '@/utils/prisma';
+
+export const getUserSubscription = async(userId: string) => {
+	if(!userId) throw new Error('No userId provided')
+
+	return await prisma.subscription.findUnique({
+		where: {
+			userId: userId
+		}
+	})
+}
