4aeeaf0168d10b35ebf2f4ae8e0e3286be1d852f
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-19 22:19:28 +0100

    feat: start of adding early access holding page

--- apps/marketing/app/layout.tsx
+++ apps/marketing/app/layout.tsx
@@ -1,4 +1,43 @@
+import './styles/globals.css';
+import { Cairo, Poppins, Kanit, Inter } from 'next/font/google';
+import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
+import SiteLogo from '@/components/logo';
+
+const CairoFont = Cairo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-cairo',
+  style: 'normal'
+});
+
+const PoppinsFont = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  style: 'normal',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+const KanitFont = Kanit({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-kanit',
+  style: 'normal',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+const InterFont = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+  style: 'normal',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const metadata = {
+  title: 'Dashboard | Draggle'
+};
 
 export default function RootLayout({
   children
@@ -6,14 +45,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${CairoFont.variable} ${PoppinsFont.variable} ${KanitFont.variable} ${InterFont.variable}`}
+    >
+      {/* className="bg-gradient-to-b from-black-200 to-electric-violet-950  min-h-screen" */}
       <body
-        className="bg-gradient-to-b from-black-200 to-electric-violet-950 min-h-screen"
+        className="bg-[#05050A] min-h-screen max-h-screen flex justify-center items-center"
         suppressHydrationWarning={true}
       >
+        {/* <Theme className="bg-grid-white/[0.03]"> */}
         <Theme>
-          <div className="flex flex-col px-20">
-            <div>{children}</div>
+          {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div> */}
+          <div className="flex flex-col relative">
+            <header className="flex items-center justify-between p-7 w-full text-white absolute top-0 z-30">
+              <SiteLogo
+                logoSize="size-7"
+                className="text-xl"
+              />
+            </header>
+            <main>{children}</main>
           </div>
         </Theme>
       </body>
--- apps/marketing/app/page.tsx
+++ apps/marketing/app/page.tsx
@@ -1,3 +1,64 @@
+'use client';
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { BackgroundBeams } from '@/components/ui/background-beams';
+import { DotBackgroundDemo } from '@/components/ui/background-dots';
+import Typewriter from 'typewriter-effect';
+
 export default function App() {
-  return <div>Test.</div>;
+  return (
+    <div className="min-w-[100vw] mx-auto h-screen overflow-hidden">
+      <div className="min-h-full w-full bg-[#05050A] bg-dot-white/[0.1] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#05050A] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+        <div className="flex items-center flex-col justify-center px-3.5 md:px-10  py-4 w-full h-full font-inter">
+          <h2 className="text-white text-4xl md:text-6xl font-bold text-center z-20">
+            From idea to product, <br />
+            <span className="flex items-center gap-x-3 justify-center mt-2">
+              in
+              <Typewriter
+                options={{
+                  strings: ['hours', 'minutes', 'seconds'],
+                  autoStart: true,
+                  loop: true,
+                  deleteSpeed: 50
+                }}
+              />
+            </span>
+          </h2>
+          <p className="text-white text-center mt-4 text-sm md:text-lg font-poppins w-3/5 md:w-auto">
+            A drag and drop website builder, that{' '}
+            <span className="font-bold">actually </span>
+            requires no code.
+          </p>
+          <div className="flex flex-col gap-y-6 justify-center text-white z-20 w-full md:w-auto">
+            <div className="flex flex-col md:flex-row gap-y-4 mt-8 items-center gap-x-4 self-center px-4 w-full md:w-auto">
+              <Input
+                type="email"
+                placeholder="Email"
+                className="
+              bg-transparent min-w-full border-white p-2 md:min-w-80 md:min-h-full
+              focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50
+              text-white placeholder:text-white/85 placeholder:font-poppins placeholder:!leading-relaxed
+              md:max-w-40 
+            "
+              />
+              <Button
+                variant="secondary"
+                size="lg"
+                className="w-full md:w-auto"
+              >
+                Sign up
+              </Button>
+            </div>
+            <h6 className="font-inter md:text-xl text-center">
+              Sign up for early access and get{' '}
+              <span className="font-bold">50%</span> off your first month.
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
