e26844d314b279e2ebd358c36e33d01d51f744e4
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-19 23:03:32 +0100

    style: minor style amends

--- apps/marketing/app/page.tsx
+++ apps/marketing/app/page.tsx
@@ -11,7 +11,7 @@ export default function App() {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#05050A] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
         <div className="flex items-center flex-col justify-center px-3.5 md:px-10  py-4 w-full h-full font-inter">
-          <h2 className="text-white text-4xl md:text-6xl font-bold text-center z-20">
+          <h2 className="text-white text-4xl md:text-6xl font-bold text-center z-20 bg-gradient-to-r from-white to-gray-500 text-transparent bg-clip-text">
             From idea to product, <br />
             <span className="flex items-center gap-x-3 justify-center mt-2">
               in
@@ -28,22 +28,28 @@ export default function App() {
             </span>
           </h2>
           <p className="text-white text-center mt-4 text-sm md:text-lg font-poppins w-3/5 md:w-auto">
-            A drag and drop website builder, that{' '}
+            A drag and drop website builder, that{'  '}
             <span className="font-semibold text-electric-violet italic">
               actually{'  '}
             </span>
+            {'  '}
             requires no code.
           </p>
           <div className="flex flex-col gap-y-6 justify-center text-white z-20 w-full md:w-auto">
-            <div className="flex flex-col md:flex-row gap-y-4 mt-8 items-center gap-x-4 self-center px-4 w-full md:w-auto">
+            <div
+              className="
+                flex flex-col md:flex-row gap-y-4 mt-8 items-center gap-x-4 self-center w-full md:w-auto
+                md:bg-black p-2 border-none md:!border-solid md:border md:!border-white/20 rounded-lg
+              "
+            >
               <Input
                 type="email"
                 placeholder="Email"
                 className="
-                  bg-black min-w-full border-white p-2 md:min-w-80 md:min-h-full
+                  min-w-full border-none p-2 md:min-w-80 md:min-h-full bg-black
                   focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50
                   text-white placeholder:text-white/85 placeholder:font-poppins placeholder:!leading-relaxed
-                  md:max-w-40 
+                  md:max-w-40 text-lg border-white/20 md:border-none h-14 md:h-10
                 "
               />
               <Button
@@ -56,7 +62,8 @@ export default function App() {
             </div>
             <h6 className="font-inter md:text-xl text-center">
               Sign up for early access and get{' '}
-              <span className="font-bold">50%</span> off your first month.
+              <span className="font-bold text-electric-violet">50%</span> off
+              your first month.
             </h6>
           </div>
         </div>
