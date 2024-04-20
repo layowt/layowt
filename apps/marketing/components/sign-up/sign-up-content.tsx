4c10db39b28b3f5fd4d5b9702fc0e0de8aa3c0b0
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-20 22:27:46 +0100

    fix: separating files into components
    bcos clean codebase = cool right?

---
+++ apps/marketing/components/sign-up/sign-up-content.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { useState } from 'react';
+import { signUp } from '@/utils/sign-up';
+
+import { Input } from '~/components/ui/input';
+import { Button } from '~/components/ui/button';
+import { toast } from 'sonner';
+
+export default function SignUpContent() {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSignUp = async () => {
+    try {
+      await signUp(email);
+      toast('Signed up successfully! 🎉');
+    } catch (e) {
+      console.error(e);
+      toast('An error occurred. Please try again.', {
+        description: e.message
+      });
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-y-6 justify-center text-white z-20 w-full md:w-auto">
+      <form
+        className="
+					flex flex-col md:flex-row gap-y-4 mt-8 items-center gap-x-4 self-center w-full md:w-auto
+					md:bg-black p-2 border-none md:!border-solid md:border md:!border-white/20 rounded-lg
+				"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSignUp();
+        }}
+      >
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          className="
+						min-w-full border-none p-2 md:min-w-80 md:min-h-full bg-black
+						focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50
+						text-white placeholder:text-white/85 placeholder:font-poppins placeholder:!leading-relaxed
+						md:max-w-40 text-lg border-white/20 md:border-none h-14 md:h-10
+					"
+        />
+        <Button
+          variant="secondary"
+          size="lg"
+          className="w-full md:w-auto text-lg"
+          type="submit"
+        >
+          Sign up
+        </Button>
+      </form>
+      <h6 className="font-inter md:text-xl text-center">
+        Sign up for early access and get{' '}
+        <span className="font-bold text-electric-violet">50%</span> off your
+        first month.
+      </h6>
+    </div>
+  );
+}
