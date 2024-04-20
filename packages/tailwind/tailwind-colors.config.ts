f9ca191a0fd5868d115e5a8066f536a73a709c26
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-20 11:45:28 +0100

    fix: separate tailwind colors to own config file

---
+++ packages/tailwind/tailwind-colors.config.ts
@@ -0,0 +1,69 @@
+module.exports = {
+	colors: {
+		border: 'hsl(var(--border))',
+		input: 'hsl(var(--input))',
+		ring: 'hsl(var(--ring))',
+		background: 'hsl(var(--background))',
+		foreground: 'hsl(var(--foreground))',
+		primary: {
+			DEFAULT: 'hsl(var(--primary))',
+			foreground: 'hsl(var(--primary-foreground))'
+		},
+		secondary: {
+			DEFAULT: 'hsl(var(--secondary))',
+			foreground: 'hsl(var(--secondary-foreground))'
+		},
+		'electric-violet': {
+			DEFAULT: '#6725F2',
+			50: '#E0D2FC',
+			100: '#D2BFFB',
+			200: '#B899F9',
+			300: '#7c57cd',
+			400: '#824CF4',
+			500: '#6725F2',
+			600: '#4C0DD2',
+			700: '#39099D',
+			800: '#260668',
+			900: '#05010E',
+			950: '#070113'
+		},
+		white: {
+			DEFAULT: '#EBEBEB'
+		},
+		black: {
+			DEFAULT: '#0E0E0E',
+			50: '#2D2D2D',
+			75: '#131313',
+			100: '#111111',
+			200: '#0E0E0E',
+			300: '#000000',
+			400: '#000000',
+			500: '#000000',
+			600: '#000000',
+			700: '#000000',
+			800: '#000000',
+			900: '#000000',
+			950: '#000000'
+		},
+		destructive: {
+			DEFAULT: 'hsl(var(--destructive))',
+			foreground: 'hsl(var(--destructive-foreground))'
+		},
+		muted: {
+			DEFAULT: 'hsl(var(--muted))',
+			foreground: 'hsl(var(--muted-foreground))'
+		},
+		accent: {
+			DEFAULT: 'hsl(var(--accent))',
+			foreground: 'hsl(var(--accent-foreground))'
+		},
+		popover: {
+			DEFAULT: 'hsl(var(--popover))',
+			foreground: 'hsl(var(--popover-foreground))'
+		},
+		card: {
+			DEFAULT: 'hsl(var(--card))',
+			foreground: 'hsl(var(--card-foreground))'
+		}
+	},
+}
--- packages/tailwind/tailwind.config.ts
+++ packages/tailwind/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const svgToDataUri = require("mini-svg-data-uri");
+const tailwindColors = require('./tailwind-colors.config')
 
 const {
   default: flattenColorPalette,
@@ -39,73 +40,7 @@ const sharedConfig = {
         kanit: ['var(--font-kanit)'],
         inter: ['var(--font-inter)']
       },
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        'electric-violet': {
-          DEFAULT: '#6725F2',
-          50: '#E0D2FC',
-          100: '#D2BFFB',
-          200: '#B899F9',
-          300: '#7c57cd',
-          400: '#824CF4',
-          500: '#6725F2',
-          600: '#4C0DD2',
-          700: '#39099D',
-          800: '#260668',
-          900: '#05010E',
-          950: '#070113'
-        },
-        white: {
-          DEFAULT: '#EBEBEB'
-        },
-        black: {
-          DEFAULT: '#0E0E0E',
-          50: '#2D2D2D',
-          75: '#131313',
-          100: '#111111',
-          200: '#0E0E0E',
-          300: '#000000',
-          400: '#000000',
-          500: '#000000',
-          600: '#000000',
-          700: '#000000',
-          800: '#000000',
-          900: '#000000',
-          950: '#000000'
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        }
-      },
+      ...tailwindColors,
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
@@ -128,11 +63,22 @@ const sharedConfig = {
             backgroundPosition: "-200% 0",
           },
         },
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        shimmer: "shimmer 3s linear infinite"
+        shimmer: 'shimmer 3s linear infinite',
+        text: 'text 5s ease infinite',
       }
     }
   },
