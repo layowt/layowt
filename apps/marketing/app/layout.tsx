438e41a1192b9590511a8657d9524ba974ec1444
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-21 16:43:58 +0100

    feat: adds posthog to marketing site

--- apps/marketing/app/layout.tsx
+++ apps/marketing/app/layout.tsx
@@ -4,6 +4,7 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import SiteLogo from '~/components/logo';
 import { Toaster } from '~/components/ui/sonnner';
+import { CSPostHogProvider } from './providers';
 
 const CairoFont = Cairo({
   subsets: ['latin'],
@@ -50,29 +51,31 @@ export default function RootLayout({
       lang="en"
       className={`${CairoFont.variable} ${PoppinsFont.variable} ${KanitFont.variable} ${InterFont.variable}`}
     >
-      <body
-        className="bg-[#05050A] min-h-screen max-h-screen flex justify-center items-center"
-        suppressHydrationWarning={true}
-      >
-        <Theme>
-          <div className="min-h-full w-full bg-[#05050A] bg-dot-white/[0.2] relative flex items-center justify-center">
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#05050A] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <div className="flex flex-col relative">
-              <header className="flex items-center justify-between p-7 w-full text-white absolute top-0 z-30">
-                <SiteLogo
-                  logoSize="size-7"
-                  className="text-xl"
+      <CSPostHogProvider>
+        <body
+          className="bg-[#05050A] min-h-screen max-h-screen flex justify-center items-center"
+          suppressHydrationWarning={true}
+        >
+          <Theme>
+            <div className="min-h-full w-full bg-[#05050A] bg-dot-white/[0.2] relative flex items-center justify-center">
+              <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#05050A] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+              <div className="flex flex-col relative">
+                <header className="flex items-center justify-between p-7 w-full text-white absolute top-0 z-30">
+                  <SiteLogo
+                    logoSize="size-7"
+                    className="text-xl"
+                  />
+                </header>
+                <main>{children}</main>
+                <Toaster
+                  closeButton
+                  className="z-[100] group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg group-[.toaster]:pointer-events-auto"
                 />
-              </header>
-              <main>{children}</main>
-              <Toaster
-                closeButton
-                className="z-[100] group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg group-[.toaster]:pointer-events-auto"
-              />
+              </div>
             </div>
-          </div>
-        </Theme>
-      </body>
+          </Theme>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
