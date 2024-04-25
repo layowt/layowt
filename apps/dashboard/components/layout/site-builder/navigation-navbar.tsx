13d85c9e28ead816b7caa5215c61ff34f7620ddb
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-25 21:36:24 +0100

    fix: minor button change to prevent spring motion on every button instance
    also start of device swapper

--- apps/dashboard/components/layout/site-builder/navigation-navbar.tsx
+++ apps/dashboard/components/layout/site-builder/navigation-navbar.tsx
@@ -1,9 +1,16 @@
 import SiteLogo from '@/components/logo';
+import ScreenSizeSwapper from '@/components/layout/site-builder/screensize-swapper';
 
 export default function SiteBuilderNavBar() {
   return (
     <div className="p-4 border-b border-black-50">
-      <SiteLogo className="text-white" />
+      <div className="flex items-center justify-between">
+        <SiteLogo className="text-white" />
+
+        <ScreenSizeSwapper />
+
+        <div className="bg-white w-10 h-8"></div>
+      </div>
     </div>
   );
 }
