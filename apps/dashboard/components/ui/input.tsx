ce3173e62b052882cbfdda5715f479d9034e0df5
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-27 21:43:30 +0100

    style: add more custom props to input

--- apps/dashboard/components/ui/input.tsx
+++ apps/dashboard/components/ui/input.tsx
@@ -1,18 +1,68 @@
 import * as React from 'react';
 
+import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
+
+// variants of the input
+const inputVariants = cva(
+  'focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        primary: '',
+        secondary: '',
+        tertiary: '',
+        outline: '',
+        transparent: 'bg-transparent placeholder:text-muted-foreground'
+      },
+      size: {
+        default: 'h-10 w-56',
+        sm: 'w-32',
+        md: 'w-56',
+        lg: 'w-64',
+        xl: 'w-96',
+        wide: 'w-full'
+      },
+      underline: {
+        none: '!border-none',
+        default: '!border-b !border-b-black-75 rounded-none'
+      },
+      padding: {
+        none: '!p-0',
+        sm: 'px-2 py-1',
+        md: 'px-4 py-2',
+        lg: 'px-8 py-4'
+      },
+      color: {
+        'light-black': ''
+      }
+    },
+    defaultVariants: {
+      variant: 'primary',
+      padding: 'md',
+      size: 'default'
+    }
+  }
+);
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+export interface InputVariantProps extends VariantProps<typeof inputVariants> {}
+
+export type InputCombinedProps = InputProps & InputVariantProps;
+
+const Input = React.forwardRef<HTMLInputElement, InputCombinedProps>(
+  (
+    { className, variant, size, type, padding, underline, color, ...props },
+    ref
+  ) => {
     return (
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-          className
+          'flex h-10 w-full rounded-md bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          inputVariants({ className, padding, size, variant, underline, color })
         )}
         ref={ref}
         {...props}
