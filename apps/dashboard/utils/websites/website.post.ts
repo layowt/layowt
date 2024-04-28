659f151195be5452dcfb3aaf22e9d6c61cc7d090
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-04-28 11:53:08 +0100

    feat: move file to new dir & new util for update a site

---
+++ apps/dashboard/utils/websites/website.post.ts
@@ -0,0 +1,28 @@
+import { prisma } from '@/utils/prisma'
+import type { websites as Website } from '@prisma/client'
+
+/**
+ * Function to update a website via its id
+ * 
+ * @param options An object containing:
+ * 					- websiteId 
+ * 					- options to update
+ * @returns void
+ */
+export const updateWebsite = async (
+	websiteId: string,
+	options: Partial<Website>
+): Promise<Website> => {
+	if(!websiteId) throw new Error('No website ID specified')
+
+	// find the site that needs to be update and update it
+	const siteToUpdate = await prisma.websites.update({
+		where: {
+			websiteId
+		},
+		data: {
+			...options	
+		}
+	})
+	return siteToUpdate;
+}
