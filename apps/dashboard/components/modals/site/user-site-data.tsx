59b023a58c2f856f76d419764782e6353209430c
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-04 20:42:18 +0100

    feat: saving site data on site onboarding

--- apps/dashboard/components/modals/site/user-site-data.tsx
+++ apps/dashboard/components/modals/site/user-site-data.tsx
@@ -1,12 +1,27 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { website } from '@/store/slices/website-store';
-import type { websites as Website } from '@prisma/client';
+
+// react
 import { useState } from 'react';
-import { DialogTrigger } from '@radix-ui/react-dialog';
-import { HexColorPicker } from 'react-colorful';
+
+// components
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog';
+import { HexColorInput, HexColorPicker } from 'react-colorful';
+import { Input } from '@/components/ui/input';
+
+// redux
+import { useAppSelector } from '@/lib/hooks';
+import { website } from '@/store/slices/website-store';
+
+// misc / utils
+import type { websites as Website } from '@prisma/client';
+import { updateWebsite } from '@/utils/websites/website.post';
+import type { SavingState } from '@/types/state';
 
 type NewWebsiteData = Pick<
   Website,
@@ -17,6 +32,8 @@ type NewWebsiteData = Pick<
 >;
 
 export default function UserSiteData() {
+  const currentSite = useAppSelector(website);
+
   const [state, setState] = useState<NewWebsiteData>({
     websiteName: '',
     websiteLogo: '',
@@ -25,12 +42,23 @@ export default function UserSiteData() {
   });
   const [primaryColor, setPrimaryColor] = useState<string>('');
   const [secondaryColor, setSecondaryColor] = useState<string>('');
+  const [status, setStatus] = useState<SavingState>('idle');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setState({
       ...state,
       [e.target.name]: e.target.value
     });
+  };
+
+  const saveSiteData = async (siteId: string, Data: NewWebsiteData) => {
+    setStatus('saving');
+    try {
+      updateWebsite(siteId, Data);
+    } catch (e) {
+      setStatus('error');
+    }
+    setStatus('idle');
   };
 
   return (
@@ -47,24 +75,27 @@ export default function UserSiteData() {
       >
         <div className="flex flex-col gap-y-2">
           <div className="flex flex-col gap-y-0.5">
-            <span className="text-electric-violet text-[8px] uppercase">
-              Site Builder
-            </span>
-            <DialogTitle>
-              <h1 className="text-xl font-semibold text-white font-poppins">
-                Welcome to the site builder!
-              </h1>
+            <DialogTitle className="mt-2 grid grid-cols-12 gap-x-4 items-center">
+              <div className="bg-white size-14 rounded-full col-span-2"></div>
+              <div className="flex flex-col gap-y-1 col-span-10">
+                <h1 className="text-2xl font-semibold text-white font-poppins">
+                  Welcome to the site builder!
+                </h1>
+                <p className="text-white/80 text-xs font-poppins font-light">
+                  You're digital journey begins here, we just need a few details
+                  before we can continue.
+                </p>
+              </div>
             </DialogTitle>
           </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-white text-xs font-poppins">
-              You're digital journey begins here, we just need a few details
-              before we can continue.
-            </p>
-          </div>
-          <form className="flex flex-col gap-4 mt-6">
-            {/** Site name */}
+          <div className="flex flex-col gap-4 mt-6">
             <div className="flex flex-col gap-y-1 relative">
+              <label
+                htmlFor="websiteName"
+                className="text-white/80 text-sm pl-1"
+              >
+                Site Name: {currentSite.websiteId}
+              </label>
               <Input
                 type="text"
                 className="w-full peer"
@@ -74,28 +105,16 @@ export default function UserSiteData() {
                 onChange={handleChange}
                 name="websiteName"
               />
-              <label
-                htmlFor="websiteName"
-                className="
-									absolute -top-2 left-2.5 text-[10px] text-white/80 font-poppins
-									peer-placeholder-shown:text-xs peer-placeholder-shown:top-[12px] bg-black-75
-									peer-placeholder-shown:left-5 transition-all peer-placeholder-shown:bg-black-75 
-								"
-              >
-                Site name
-              </label>
             </div>
             {/** Site primary / secondary brand color */}
             <div className="flex gap-4 mt-2">
               <Dialog>
                 <DialogTrigger asChild>
                   <div className="flex gap-x-2 items-center hover:cursor-pointer">
-                    {/** COLOR */}
                     <div
                       className="rounded-full size-8 border border-black"
                       style={{ backgroundColor: primaryColor }}
                     ></div>
-                    {/** PRIMARY COLOR + HEX VALUE */}
                     <div className="flex flex-col gap-y-0.5">
                       <span className="text-xs">Primary Color</span>
                       <p className="font-extralight text-[10px]">
@@ -116,18 +135,21 @@ export default function UserSiteData() {
                       color={primaryColor}
                       onChange={setPrimaryColor}
                     />
+                    <HexColorInput
+                      color={primaryColor}
+                      onChange={setPrimaryColor}
+                      className="bg-black-75 border border-black-50 px-2 py-1 rounded-lg"
+                    />
                   </div>
                 </DialogContent>
               </Dialog>
               <Dialog>
                 <DialogTrigger asChild>
                   <div className="flex gap-x-2 items-center hover:cursor-pointer">
-                    {/** COLOR */}
                     <div
                       className="border border-black rounded-full size-8"
                       style={{ backgroundColor: secondaryColor }}
                     ></div>
-                    {/** PRIMARY COLOR + HEX VALUE */}
                     <div className="flex flex-col gap-y-0.5">
                       <span className="text-xs">Secondary Color</span>
                       <p className="font-extralight text-[10px]">
@@ -148,6 +170,11 @@ export default function UserSiteData() {
                       color={secondaryColor}
                       onChange={setSecondaryColor}
                     />
+                    <HexColorInput
+                      color={secondaryColor}
+                      onChange={setSecondaryColor}
+                      className="bg-black-75 border border-black-50 px-2 py-1 rounded-lg"
+                    />
                   </div>
                 </DialogContent>
               </Dialog>
@@ -164,10 +191,15 @@ export default function UserSiteData() {
                 >
                   Skip for now
                 </Button>
-                <Button variant="secondary">Save</Button>
+                <Button
+                  variant="secondary"
+                  onClick={() => saveSiteData(currentSite.websiteId, state)}
+                >
+                  Save
+                </Button>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
---
+++ apps/dashboard/types/state.ts
@@ -0,0 +1,1 @@
+export type SavingState = 'idle' | 'saving' | 'error'
