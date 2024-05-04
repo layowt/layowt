21046049aa135e960a674ad2f891a6ccdf55316a
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-04 21:14:35 +0100

    feat: componentise the site onboarding modal a bit more

--- apps/dashboard/components/modals/site/user-site-data.tsx
+++ apps/dashboard/components/modals/site/user-site-data.tsx
@@ -5,14 +5,9 @@ import { useState } from 'react';
 
 // components
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog';
-import { HexColorInput, HexColorPicker } from 'react-colorful';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import SiteOnboardingTitle from './modal-title';
 
 // redux
 import { useAppSelector } from '@/lib/hooks';
@@ -22,6 +17,8 @@ import { website } from '@/store/slices/website-store';
 import type { websites as Website } from '@prisma/client';
 import { updateWebsite } from '@/utils/websites/website.post';
 import type { SavingState } from '@/types/state';
+import ModalPrimaryColor from './modal-primary-color';
+import ModalSecondaryColor from './modal-secondary-color';
 
 type NewWebsiteData = Pick<
   Website,
@@ -40,8 +37,7 @@ export default function UserSiteData() {
     websitePrimaryColor: '',
     websiteSecondaryColor: ''
   });
-  const [primaryColor, setPrimaryColor] = useState<string>('');
-  const [secondaryColor, setSecondaryColor] = useState<string>('');
+
   const [status, setStatus] = useState<SavingState>('idle');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -51,10 +47,24 @@ export default function UserSiteData() {
     });
   };
 
+  const setPrimaryColor = (color: string) => {
+    setState({
+      ...state,
+      websitePrimaryColor: color
+    });
+  };
+
+  const setSecondaryColor = (color: string) => {
+    setState({
+      ...state,
+      websiteSecondaryColor: color
+    });
+  };
+
   const saveSiteData = async (siteId: string, Data: NewWebsiteData) => {
     setStatus('saving');
     try {
-      updateWebsite(siteId, Data);
+      await updateWebsite(siteId, Data);
     } catch (e) {
       setStatus('error');
     }
@@ -74,20 +84,7 @@ export default function UserSiteData() {
         }}
       >
         <div className="flex flex-col gap-y-2">
-          <div className="flex flex-col gap-y-0.5">
-            <DialogTitle className="mt-2 grid grid-cols-12 gap-x-4 items-center">
-              <div className="bg-white size-14 rounded-full col-span-2"></div>
-              <div className="flex flex-col gap-y-1 col-span-10">
-                <h1 className="text-2xl font-semibold text-white font-poppins">
-                  Welcome to the site builder!
-                </h1>
-                <p className="text-white/80 text-xs font-poppins font-light">
-                  You're digital journey begins here, we just need a few details
-                  before we can continue.
-                </p>
-              </div>
-            </DialogTitle>
-          </div>
+          <SiteOnboardingTitle />
           <div className="flex flex-col gap-4 mt-6">
             <div className="flex flex-col gap-y-1 relative">
               <label
@@ -108,76 +105,14 @@ export default function UserSiteData() {
             </div>
             {/** Site primary / secondary brand color */}
             <div className="flex gap-4 mt-2">
-              <Dialog>
-                <DialogTrigger asChild>
-                  <div className="flex gap-x-2 items-center hover:cursor-pointer">
-                    <div
-                      className="rounded-full size-8 border border-black"
-                      style={{ backgroundColor: primaryColor }}
-                    ></div>
-                    <div className="flex flex-col gap-y-0.5">
-                      <span className="text-xs">Primary Color</span>
-                      <p className="font-extralight text-[10px]">
-                        {primaryColor}
-                      </p>
-                    </div>
-                  </div>
-                </DialogTrigger>
-                <DialogContent
-                  showCloseButton={true}
-                  onInteractOutside={(e) => {
-                    e.preventDefault();
-                  }}
-                  className="bg-black-75 border border-black-50 focus:outline-none rounded-lg shadow-lg sm:rounded-lg p-10 w-fit"
-                >
-                  <div className="flex flex-col gap-y-4">
-                    <HexColorPicker
-                      color={primaryColor}
-                      onChange={setPrimaryColor}
-                    />
-                    <HexColorInput
-                      color={primaryColor}
-                      onChange={setPrimaryColor}
-                      className="bg-black-75 border border-black-50 px-2 py-1 rounded-lg"
-                    />
-                  </div>
-                </DialogContent>
-              </Dialog>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <div className="flex gap-x-2 items-center hover:cursor-pointer">
-                    <div
-                      className="border border-black rounded-full size-8"
-                      style={{ backgroundColor: secondaryColor }}
-                    ></div>
-                    <div className="flex flex-col gap-y-0.5">
-                      <span className="text-xs">Secondary Color</span>
-                      <p className="font-extralight text-[10px]">
-                        {secondaryColor}
-                      </p>
-                    </div>
-                  </div>
-                </DialogTrigger>
-                <DialogContent
-                  showCloseButton={true}
-                  onInteractOutside={(e) => {
-                    e.preventDefault();
-                  }}
-                  className="bg-black-75 border border-black-50 focus:outline-none rounded-lg shadow-lg sm:rounded-lg p-10 w-fit"
-                >
-                  <div className="flex flex-col gap-y-4">
-                    <HexColorPicker
-                      color={secondaryColor}
-                      onChange={setSecondaryColor}
-                    />
-                    <HexColorInput
-                      color={secondaryColor}
-                      onChange={setSecondaryColor}
-                      className="bg-black-75 border border-black-50 px-2 py-1 rounded-lg"
-                    />
-                  </div>
-                </DialogContent>
-              </Dialog>
+              <ModalPrimaryColor
+                primaryColor={state.websitePrimaryColor}
+                onColorChange={(color) => setPrimaryColor(color)}
+              />
+              <ModalSecondaryColor
+                secondaryColor={state.websiteSecondaryColor}
+                onColorChange={(color) => setSecondaryColor(color)}
+              />
             </div>
             {/** The user has to enter the site name in order to continue */}
             <div className="w-full flex justify-between items-center mt-2">
