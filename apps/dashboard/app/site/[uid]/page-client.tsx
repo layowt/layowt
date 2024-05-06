52aa8b2a0863d27aa29025acecba391158d04130
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-06 21:13:23 +0100

    fix: show site modal on initial load

--- apps/dashboard/app/site/[uid]/page-client.tsx
+++ apps/dashboard/app/site/[uid]/page-client.tsx
@@ -20,11 +20,11 @@ export default async function SiteBuilderClient({
   setWebsite(website);
 
   // if we do not have a site name, try to fetch the site from the db
-  if (website.websiteName.length < 1) {
+  if (website.websiteName === 'Untitled') {
     const site = await getWebsite<Website>({ websiteId: website.websiteId });
 
     // if we do not have the data that we need - we need to render the user-site-data component
-    if (!site || !site.websiteName) {
+    if (!site || site.websiteName === 'Untitled') {
       return <NoSSR />;
     } else {
       // otherwise we're all good - set the website in redux so we can use on all
