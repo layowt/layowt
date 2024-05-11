c28cc65fae63f8779177e5f2022041b62f0522c1
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-11 20:03:02 +0100

    fix build

--- apps/dashboard/components/layout/dashboard/website-card.tsx
+++ apps/dashboard/components/layout/dashboard/website-card.tsx
@@ -25,7 +25,6 @@ export default function WebsiteCard(website: Website, index: number) {
         key={website.websiteId}
         className="flex flex-col gap-y-2 border border-black-50 bg-black-75 rounded-lg p-5 w-full relative group"
         href={`/site/${website.websiteId}`}
-        prefetch
       >
         <div className="absolute top-3 left-3 border-4 border-black-75 rounded-sm">
           {website.websiteLogo ? (
