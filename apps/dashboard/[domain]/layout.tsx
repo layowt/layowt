f5beecc241057e4e125ede20b0d5bc97d7cb7c43
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-12 16:29:38 +0100

    fix: move domain route out of app & middleware changes

--- apps/dashboard/[domain]/layout.tsx
+++ apps/dashboard/[domain]/layout.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import Loading from '../loading';
+import Loading from '../app/loading';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
