f3ddd9b365174b0c6f4283d0714d39b70212aa61
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-12 16:29:38 +0100

    fix: move domain route out of app & middleware changes

--- apps/dashboard/middleware.ts
+++ apps/dashboard/middleware.ts
@@ -2,17 +2,46 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { updateSession } from './utils/supabase/middleware';
 
 // runs on every request
-// TODO: make this update the redux user?
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  const publicRootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+  let publicRootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+
+  if(!publicRootDomain) {
+    publicRootDomain = 'app.layout.com'
+  }
 
   let hostname = req.headers.get('host')!.replace(
     'localhost:4343', publicRootDomain  
   )
-  console.log('hostname', hostname);
-  
-  return await updateSession(req);
+  const searchParams = req.nextUrl.searchParams.toString()
+
+  const path = `${url.pathname}${
+    searchParams.toString().length > 0 ? `?${searchParams.toString()}` : ''
+  }`
+
+  // if we are developing locally or on the root domain, do not redirect
+  if(hostname === 'localhost:4343' || hostname === publicRootDomain) {
+    // if we are on the root domain, we need to do user auth checks
+    const session = await updateSession(req)
+
+    // if there is no user, and they are trying to access a page that requires auth
+    // redirect them
+    if(
+        !session && 
+        path !== '/login' && 
+        path !== '/signup' && 
+        path !== '/forgot-password'
+    ){
+      return NextResponse.redirect('/login')
+    }
+
+    // other wise, the user is authenticated, trying to access the root site (app.layowt.com), and is allowed to access the page
+    return NextResponse.next()
+  }
+
+  return NextResponse.rewrite(
+    new URL(`${hostname}${path}`, req.url)
+  )
 }
 
 export const config = {
