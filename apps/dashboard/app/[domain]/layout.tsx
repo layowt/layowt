b7370e108bfb3a098dc96df7e2a8dbaa1905924e
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-12 18:50:53 +0100

    feat: extremely minimal working example!

--- apps/dashboard/app/[domain]/layout.tsx
+++ apps/dashboard/app/[domain]/layout.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import Loading from '../app/loading';
+import Loading from '../loading';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
