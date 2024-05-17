a2b420a8fe8c88521bd150118c86ffe81fb95e3a
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-17 16:15:21 +0100

    fix: layowt in pricing

--- apps/dashboard/app/[domain]/page.tsx
+++ apps/dashboard/app/[domain]/page.tsx
@@ -2,8 +2,13 @@ import { prisma } from '@/utils/prisma';
 import { notFound } from 'next/navigation';
 
 import { getDynamicSite } from '@/utils/websites';
+import { Metadata } from 'next';
 
-//export async function generateStaticParams() {}
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: ''
+  }
+}
 
 export default async function Page({ params }: { params: { domain: string } }) {
   const domain = decodeURIComponent(params.domain);
