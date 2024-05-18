1456bb26f818480328dad8406a0b9cea3779caaa
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-18 21:21:41 +0100

    style-fix: correct bg color

--- apps/dashboard/app/[domain]/admin/layout.tsx
+++ apps/dashboard/app/[domain]/admin/layout.tsx
@@ -4,7 +4,7 @@ export default function LoginLayout({
   children: React.ReactNode;
 }){
   return (
-    <div className="min-h-full w-full bg-grid-small-white/5 relative flex items-center justify-center">
+    <div className="bg-[#05050A] min-h-full w-full bg-grid-small-white/5 relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#05050A] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
       {children}
     </div>
