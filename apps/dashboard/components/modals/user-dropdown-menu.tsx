41acad8e3bd54eb6c4f3e419812a54f1c23b2afa
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-19 20:14:21 +0100

    style: w-60 -> w-72

--- apps/dashboard/components/modals/user-dropdown-menu.tsx
+++ apps/dashboard/components/modals/user-dropdown-menu.tsx
@@ -128,7 +128,7 @@ export default function UserDropdownMenu({
             </button>
           )}
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="font-inter border border-black-50 !bg-black text-white/80 m-1 w-60">
+      <DropdownMenuContent className="font-inter border border-black-50 !bg-black text-white/80 m-1 w-72">
         <motion.div
           initial={{ opacity: 0, x: 0 }}
           animate={{ opacity: 1, x: 0 }}
