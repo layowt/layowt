5b681e3724b066a0805e605fb94061f6af430e32
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-05-29 20:39:27 +0100

    feat: adding second design to site nav

--- apps/dashboard/components/layout/site-builder/site-builder-options.tsx
+++ apps/dashboard/components/layout/site-builder/site-builder-options.tsx
@@ -1,7 +1,10 @@
-import { GearIcon, LayoutIcon } from "@radix-ui/react-icons"
+import { GearIcon, LayoutIcon, FileIcon } from "@radix-ui/react-icons"
 
-export default function SiteBuilderOptions(){
+export default function SiteBuilderOptions() {
   const items = [
+    {
+      icon: <FileIcon className="size-4" />,
+    },
     {
       icon: <LayoutIcon className="size-4" />,
     },  
@@ -11,12 +14,46 @@ export default function SiteBuilderOptions(){
   ]
 
   return (
-    <div className="px-2 py-4 w-12 h-full border-r border-black-50 flex flex-col gap-y-2">
-      {items.map((item, index) => (
-        <div key={index} className="flex justify-center items-center w-full p-2 rounded-lg">
-          {item.icon}
+    <>
+      {/* <div 
+        className="flex h-full relative" 
+        style={{ top: 'calc(50% - 73px)' }}
+        >
+        <div 
+          className="
+          px-2 py-4 w-11 border border-l-0 border-black-50 flex flex-col 
+          gap-y-3 rounded-tr-lg rounded-br-lg h-fit
+          "
+          >
+          {items.map((item, index) => (
+            <button 
+            key={index} 
+            className="flex justify-center items-center w-full p-1 rounded-lg"
+            >
+              {item.icon}
+            </button>
+          ))}
         </div>
-      ))}
-    </div>
+      </div> */}
+      <div 
+        className="flex h-full"
+      >
+        <div 
+          className="
+            px-2 py-4 w-11 border-r border-black-50 flex flex-col 
+            gap-y-3
+          "
+        >
+          {items.map((item, index) => (
+            <button 
+              key={index} 
+              className="flex justify-center items-center w-full p-1 rounded-lg"
+            >
+              {item.icon}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
