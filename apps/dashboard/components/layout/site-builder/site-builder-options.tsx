a5311a0f95c920d35ffa0a21f003740046ca3fc3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-06-01 15:55:10 +0100

    feat: move 'createQueryParam' to custom hook

--- apps/dashboard/components/layout/site-builder/site-builder-options.tsx
+++ apps/dashboard/components/layout/site-builder/site-builder-options.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { GearIcon, LayoutIcon, FileIcon } from "@radix-ui/react-icons"
+import { GearIcon, LayoutIcon, FileIcon } from "@radix-ui/react-icons";
 import {
   Tooltip,
   TooltipContent,
@@ -9,106 +9,67 @@ import {
 } from '@/ui/tooltip';
 
 import { cn, useAppDispatch, useAppSelector } from "@/utils/index";
-import { currentSection, setCurrentSection, type SectionState } from '@/store/slices/website-store'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useCallback, useEffect } from "react";
+import { currentSection, setCurrentSection, type SectionState } from '@/store/slices/website-store';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from "react";
+import { useQueryParams, useCreateQueryString } from '@/hooks/useQueryParams';
 
 export default function SiteBuilderOptions() {
   const dispatch = useAppDispatch();
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   const currentSelectedSection = useAppSelector(currentSection);
+  
+  const createQueryString = useCreateQueryString();
 
-  const handleButtonClick = useCallback(
-    (section: SectionState) => {
-      dispatch(setCurrentSection(section))
-      router.push(
-        pathname + '?' + createQueryString('s', section)
-      )
-    },
-    [dispatch]
-  )
+  const handleButtonClick = useQueryParams({
+    name: 's',
+    value: currentSelectedSection
+  });
 
   // on mount set the current section
   useEffect(() => {
-    const section = searchParams.get('s') as SectionState
+    const section = searchParams.get('s') as SectionState;
 
     if (section) {
-      dispatch(setCurrentSection(section))
+      dispatch(setCurrentSection(section));
       return;
     }
-    router.push(pathname + '?' + createQueryString('s', currentSelectedSection))
-  }, [])
+    router.push(pathname + '?' + createQueryString('s', 'pages'));
+  }, [dispatch, router, pathname, searchParams, createQueryString]);
 
   const items = [
     {
       name: 'pages',
       icon: <FileIcon className="size-4" />,
       toolTip: 'Pages',
-      onClick: () =>  handleButtonClick('pages')
+      onClick: () => handleButtonClick('pages')
     },
     {
       name: 'layout',
       icon: <LayoutIcon className="size-4" />,
       toolTip: 'Layout',
       onClick: () => handleButtonClick('layout')
-    },  
+    },
     {
       name: 'settings',
       icon: <GearIcon className="size-4" />,
       toolTip: 'Settings',
       onClick: () => handleButtonClick('settings')
     }
-  ]
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
- 
-      return params.toString()
-    },
-    [searchParams]
-  )
+  ];
 
   return (
     <>
-      {/* <div 
-        className="flex h-full relative" 
-        style={{ top: 'calc(50% - 73px)' }}
-        >
-        <div 
-          className="
-          px-2 py-4 w-11 border border-l-0 border-black-50 flex flex-col 
-          gap-y-3 rounded-tr-lg rounded-br-lg h-fit
-          "
-          >
+      <div className="flex h-full">
+        <div className="px-2 py-4 w-12 border-r border-black-50 flex flex-col gap-y-3">
           {items.map((item, index) => (
-            <button 
-            key={index} 
-            className="flex justify-center items-center w-full p-1 rounded-lg"
-            >
-              {item.icon}
-            </button>
-          ))}
-        </div>
-      </div> */}
-      <div 
-        className="flex h-full"
-      >
-        <div 
-          className="px-2 py-4 w-12 border-r border-black-50 flex flex-col gap-y-3"
-        >
-          {items.map((item, index) => (
-            <TooltipProvider 
-              key={index} 
-              delayDuration={0}
-            >
+            <TooltipProvider key={index} delayDuration={0}>
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <button 
+                  <button
                     className={cn(
                       'flex justify-center items-center w-full px-1 py-2 rounded-md',
                       currentSelectedSection === item.name ? 'bg-black-50' : ''
@@ -118,7 +79,7 @@ export default function SiteBuilderOptions() {
                     {item.icon}
                   </button>
                 </TooltipTrigger>
-                <TooltipContent  side="right" className="bg-black-75 border border-black-50 z-[100]">
+                <TooltipContent side="right" className="bg-black-75 border border-black-50 z-[100]">
                   {item.toolTip}
                 </TooltipContent>
               </Tooltip>
@@ -127,5 +88,5 @@ export default function SiteBuilderOptions() {
         </div>
       </div>
     </>
-  )
+  );
 }
---
+++ apps/dashboard/hooks/useQueryParams.ts
@@ -0,0 +1,43 @@
+import { useCallback } from "react";
+import { useAppDispatch } from "../utils";
+import { SectionState, setCurrentSection } from '@/store/slices/website-store'
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+type Props = {
+  name: string;
+  value: string;
+}
+
+const useCreateQueryString = () => {
+  const searchParams = useSearchParams();
+
+  return useCallback(
+    (name: string, value: string) => {
+      const newSearchParams = new URLSearchParams(searchParams.toString());
+      newSearchParams.set(name, value);
+      return newSearchParams.toString();
+    },
+    [searchParams]
+  );
+}
+
+const useQueryParams = ({ name }: Props) => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const pathname = usePathname();
+  const createQueryString = useCreateQueryString();
+
+  const handleButtonClick = useCallback(
+    (section: SectionState) => {
+      dispatch(setCurrentSection(section))
+      router.push(
+        pathname + '?' + createQueryString(name, section)
+      )
+    },
+    [dispatch, router, pathname, createQueryString, name]
+  );
+
+  return handleButtonClick;
+}
+
+export { useCreateQueryString, useQueryParams };
