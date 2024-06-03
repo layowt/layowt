4a1cf160628bd01fa8f98fbaaff02d10ff8b26a1
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-06-03 16:58:21 +0100

    fix: accordion styles & start of adding website data to
    allow it to be changed

--- apps/dashboard/components/layout/site-builder/sections/settings.tsx
+++ apps/dashboard/components/layout/site-builder/sections/settings.tsx
@@ -4,20 +4,61 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { useAppSelector } from "@/utils/index";
+import { website } from "@/store/slices/website-store";
 
 export default function SiteBuilderSettingsSection(){
+  const site = useAppSelector(website)
+
+  const sections = [
+    {
+      title: "Colors",
+      items: [
+        {
+          title: "Primary Color",
+          content: site?.websitePrimaryColor,
+          description: "The primary color of the site"
+        },
+        {
+          title: "Secondary Color",
+          content: site?.websiteSecondaryColor,
+          description: "The secondary color of the site"
+        }
+      ]
+    },
+    {
+      title: "Favicon",
+      items: [
+        {
+          title: "Upload Favicon",
+          description: "Upload a favicon for the site"
+        }
+      ]
+    }
+  ]
   return (
-    <div className="flex flex-col gap-y-5">
+    <div className="flex flex-col gap-y-3">
       <h2 className="text-heading-xl">
         Site Settings
       </h2>
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
+      <Accordion type="single" collapsible className="overflow-y-scroll">
+        {sections.map((section, index) => (
+          <AccordionItem key={index} value={`item-${index}`} className="border-b border-black-50">
+            <AccordionTrigger className="hover:no-underline font-bold">
+              {section.title}
+            </AccordionTrigger>
+            {section.items.map((item, index) => (
+              <AccordionContent key={index} className="text-xs flex w-full justify-between">
+                <span>
+                  {item.title}
+                </span>
+                <span>
+                  {item.content}
+                </span>
+              </AccordionContent>
+            ))}
+          </AccordionItem>
+        ))}
       </Accordion>
     </div>
   )
