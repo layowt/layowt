95cfdfde0a5b81956e154fc0172500efc0c7c051
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-06-17 20:55:19 +0100

    build

--- apps/dashboard/components/layout/site-builder/sections/insert.tsx
+++ apps/dashboard/components/layout/site-builder/sections/insert.tsx
@@ -14,6 +14,7 @@ export default function Insert(){
       </div>
       {comps.map((component) => {
         return (
+          <div className="">{component.name}</div>
           // <BaseComponent 
           //   key={component.name} 
           //   component={component} 
