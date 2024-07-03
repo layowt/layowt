d0b80a939675058a2ce47ffd7dba684c8f0e8c4d
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-07-03 21:45:20 +0100

    fix: welcome page cleanup

--- apps/dashboard/app/(layowt)/(onboarding)/welcome/page.tsx
+++ apps/dashboard/app/(layowt)/(onboarding)/welcome/page.tsx
@@ -1,27 +1,29 @@
-'use client'
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { ReloadIcon } from '@radix-ui/react-icons';
-import { useState } from 'react';
 import { Button } from '~/packages/components/ui/button';
 import { FlipWords } from '~/packages/components/ui/flip-words'
 
 export default function WelcomePage(){
-  const words = ['websites', 'online stores', 'businesses'];
+  const words = ['solutions', 'storefronts', 'companies'];
 
   return (
-    <div className="text-white font-satoshi flex flex-col gap-y-2 min-h-full w-full bg-black-300 bg-dot-white/[0.2] relative items-center justify-center">
+    <div className="text-white font-satoshi flex flex-col gap-y-10 min-h-full w-full bg-black-300 bg-dot-white/[0.2] relative items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-300 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <h1 className="text-4xl font-bold text-center">
-        Create exceptional 
-        <FlipWords 
-          words={words} 
-          className="text-white" 
-        /> <br />
-        effortlessly with Layowt.
-      </h1>
-      <div className="max-w-80 text-center">
-        The all-in-one platform <br /> to build visually stunning user expierences.
+      <div className="flex flex-col gap-y-4 items-center w-1/2">
+        <h1 
+          className="text-5xl font-bold text-center" 
+          aria-label="Create expeptional user expierences with Layowt."
+        >
+          <span className='inline-block'>
+            Create exceptional 
+          </span>
+          <FlipWords 
+            words={words} 
+            className="text-white" 
+          /> <br />
+          effortlessly with Layowt.
+        </h1>
+        <p className="max-w-96 text-center text-xl">
+          The all-in-one platform <br /> to build visually stunning user expierences.
+        </p>
       </div>
       <Button 
         variant='default'
