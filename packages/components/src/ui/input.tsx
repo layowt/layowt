26a30bb43e5280e8d5986c3f0292ebf82e209aa3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-07-07 22:15:51 +0100

    fix build(?)

--- packages/components/src/ui/input.tsx
+++ packages/components/src/ui/input.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/utils/index';
+import { cn } from '~/utils/src/cn';
 
 // variants of the input
 const inputVariants = cva(
