918da2c048dcca03c0461d1931ac4ba96e4e1f45
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-07-14 16:16:10 +0100

    feat(onboarding): added product & monthly dropdowns

--- apps/dashboard/components/layout/welcome/payment-plans.tsx
+++ apps/dashboard/components/layout/welcome/payment-plans.tsx
@@ -1,6 +1,8 @@
 import { m as motion } from 'framer-motion';
 import Link from 'next/link';
 import { StripeProductReturnType } from '@layowt/utils/src/get-products';
+import { useRef } from 'react';
+import { useRouter } from 'next/navigation';
 
 // components
 import Back  from '@layowt/components/src/back';
@@ -26,6 +28,14 @@ export default function WelcomePagePaymentPlans({
 }: WelcomePagePaymentPlansProps) {
   if (typeof window === 'undefined') console.log('server')
 
+  const router = useRouter();
+  // if there are no products, send the user to the dashboard page with the new-user param
+  if(!products){
+    router.push('/dashboard?q=new-user');
+  }
+  const selectedPlanId = useRef(products.monthly[0].id);
+  const selectedBillingPeriod = useRef<'month' | 'year'>('month');
+  
   return (
     <div className="px-10 flex flex-col gap-y-4">
       <Back 
@@ -53,29 +63,44 @@ export default function WelcomePagePaymentPlans({
       <form 
         className="grid grid-cols-12 gap-4 w-96"
       >
-        <span className="col-span-12 flex flex-col gap-y-4">
-          <Separator color='offWhite' />
-          <div className="">
-            <Select>
-              <SelectTrigger className="bg-black-100 border border-black-300">
-                <SelectValue placeholder="Select a plan" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            {products.monthly.map((product) => (
-              <>
-              </>
-              // <div key={product.id} className="flex flex-col gap-y-2">
-              //   <h3 className="text-lg font-semibold text-white/80">{product.name}</h3>
-              //   <p className="text-sm text-white/50">{product.description}</p>
-              // </div>
-            ))}
-          </div>
-        </span>
+        <Separator color='offWhite' className="col-span-full" />
+        <div className="col-span-full lg:col-span-7">
+          <Select>
+            <SelectTrigger className="bg-black-100 border border-black-300 font-satoshi">
+              <SelectValue placeholder={products.monthly[0].name} defaultValue={products.monthly[0].id} />
+            </SelectTrigger>
+            <SelectContent 
+              className="bg-black-100 text-white border border-black-300 font-satoshi"
+              >
+              <SelectGroup>
+                {products.monthly.map((product) => (
+                  <SelectItem value={product.id} className="flex items-center hover:bg-black-50">
+                    {product.name}
+                  </SelectItem>  
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="col-span-full lg:col-span-5">
+          <Select>
+            <SelectTrigger className="bg-black-100 border border-black-300 font-satoshi">
+              <SelectValue placeholder="Monthly" defaultValue="month" />
+            </SelectTrigger>
+            <SelectContent 
+              className="bg-black-100 text-white border border-black-300 font-satoshi"
+              >
+              <SelectGroup>
+                <SelectItem value="month" className="flex items-center hover:bg-black-50">
+                  Monthly
+                </SelectItem>
+                <SelectItem value="year" className="flex items-center hover:bg-black-50">
+                  Yearly
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
       </form>
 
       <div className="col-span-12 flex flex-col gap-y-2 text-center">
