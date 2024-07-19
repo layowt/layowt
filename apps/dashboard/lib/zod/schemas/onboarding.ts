25b4e0accea26926494c2f285b6b300bcf976f9f
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-07-19 20:58:13 +0100

    adds schema

---
+++ apps/dashboard/lib/zod/schemas/onboarding.ts
@@ -0,0 +1,22 @@
+import { z, ZodType } from "zod";
+
+interface OnboardingSchema {
+  firstName: string;
+  lastName: string;
+  displayName: string;
+}
+
+export const onboardingSchema: ZodType<OnboardingSchema> = z.object({
+  firstName: 
+    z.string()
+    .min(1, 'First name cannot be empty')
+    .max(50, 'First name cannot be longer than 50 characters'),
+  lastName: 
+    z.string()
+    .min(1, 'Last name cannot be empty')
+    .max(50, 'Last name cannot be longer than 50 characters'),
+  displayName: 
+    z.string()
+    .min(1, 'Display name cannot be empty')
+    .max(50, 'Display name cannot be longer than 50 characters')
+}) as ZodType<OnboardingSchema>;
