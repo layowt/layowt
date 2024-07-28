1f37ddafb284529a22dd6ead138ccffba1afd27a
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-07-28 20:23:55 +0100

    fix build

--- apps/dashboard/hooks/use-user.ts
+++ apps/dashboard/hooks/use-user.ts
@@ -1,9 +1,8 @@
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "@/utils/index";
 import { setUser as setStoreUser, user } from "@/store/slices/user-store";
 import { createClient } from "@/utils/supabase/client";
 import type { User } from "@supabase/supabase-js";
-import { useQuery } from "@tanstack/react-query";
-import { getClientUser } from "@/actions/user/get-user";
 
 /**
  * Hook to get the current user, whilst setting the user in the 
@@ -15,20 +14,20 @@ export const useUser = () => {
 	const dispatch = useAppDispatch();
 	const supabase = createClient();
 
-	return useQuery({
-		queryKey: ['get-user'],
-		queryFn: getClientUser,
-	})
+	const [user, setUser] = useState<User>(null);
 
-	// useEffect(() => {
-	// 	if(init) return;
-	// 	init = true;
-	// 	const getUser = async () => {
-	// 		const { data: user } = await supabase.auth.getUser();
-	// 		setUser(user.user);
+	let init = false;
+	useEffect(() => {
+		if(init) return;
+		init = true;
+		const getUser = async () => {
+			const { data: user } = await supabase.auth.getUser();
+			setUser(user.user);
 
-	// 		dispatch(setStoreUser(user.user));
-	// 	}
-	// 	getUser();
-	// }, [])
+			dispatch(setStoreUser(user.user));
+		}
+		getUser();
+	}, [])
+
+	return user;
 }
